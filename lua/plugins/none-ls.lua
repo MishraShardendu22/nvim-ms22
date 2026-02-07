a5beea21f199ac3e2a73385a8425d812ca979912
Shardendu Mishra <shardendumishrabrave@gmail.com>
2026-02-07 18:47:30 +0530

    refactor: Simplify null-ls sources configuration and remove unused formatters
    Signed-off-by: Shardendu Mishra <shardendumishrabrave@gmail.com>

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,16 +5,19 @@ return {
   config = function()
     local null_ls = require("null-ls")
 
+    local sources = {
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.black,
+    }
+
+    -- Only add eslint_d if it's available
+    if vim.fn.executable("eslint_d") == 1 then
+      table.insert(sources, null_ls.builtins.diagnostics.eslint_d)
+    end
+
     null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-      },
+      sources = sources,
     })
 
     vim.keymap.set("n", "<leader>gf", function()
